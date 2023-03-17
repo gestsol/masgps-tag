055bc9969605a9416a8e6515c8016364e1a73e3b
Dorian <dorian.celu@gmail.com>
2023-03-17 12:14:53 -0300

    tarifas

---
+++ tarifas/tag.php
@@ -0,0 +1,43 @@
+<?php 
+
+include "./conexion/conexion.php";
+
+
+
+$sql=" select * from eventos" ;
+
+$query  = mysqli_query($mysqli, $sql);
+
+while ($row = mysqli_fetch_array($query)) { // busca en la bbdd los eventos y trae los datos
+
+     $id=$row['id'];
+     $fecha=$row['fecha'];
+     $hora=$row['hora'];
+     $portico=$row['id_portico'];
+     $categoria=$row['categoria'];
+     $autopista=$row['autopista'];
+     $patente=$row['patente'];
+     $Ndia = (date('N', strtotime($fecha))) ;
+       
+        $sql0="SELECT * FROM `festivos` WHERE dia='$fecha'"; // consulta  si la fecha corresponde a un dia festivo
+        $query0  = mysqli_query($mysqli, $sql0);
+
+        if($row = mysqli_fetch_array($query0)){
+
+             $dia="S";
+        }else{
+
+            switch ($Ndia){
+                case ($Ndia<= 5):  $dia="L-V";  // consulta si la fecha es un dia habil
+                break;
+                case (6):  $dia ="S"; // consulta si la fecha es un dia Sabado
+                break;
+                case (7):  $dia="D"; // consulta si la fecha es un dia Domingo
+                break;
+            }
+        
+            
+        }
+
+        include"./tarifa.php"; // consulta las tarifas correspondiente si es un dia habil, sabdo/festivo o domingo
+    }
---
+++ tarifas/tarifa.php
@@ -0,0 +1,69 @@
+<?php 
+
+
+
+$sql2="SELECT * FROM `horario` WHERE `id-portico`='$portico' and `dia`='$dia'";  // consulta la tarifa del portico por el dia de uso
+
+
+$query2  = mysqli_query($mysqli, $sql2);
+
+
+
+
+if($query2->num_rows==0){  // revisa que exista tarifa de punta para ese dia 
+    
+    $tarifa='normal';
+     $tarifa;
+
+
+}else{
+
+while ($row = mysqli_fetch_array($query2)){
+
+
+     $h_inicio= $row['h-inicio'];
+     $h_final= $row['h-final'];
+     $tarifa= $row['tarifa'];
+   
+   
+
+    if( $hora>=$h_inicio && $hora<=$h_final){  // compara la  hora del evento para saber si es horario normal o de punta
+
+       
+         $tarifa;
+        break;
+
+    }else {
+       
+        $tarifa='normal';
+         $tarifa;
+    }
+}
+}
+
+$sql3="SELECT * FROM `tarifa` WHERE `id_portico`='$portico' and `tarifa`='$tarifa'and`categoria`=$categoria" ; // consulta el valor de la tarifa de acuerdo a la categoria del vehiculo
+
+
+
+$query3  = mysqli_query($mysqli, $sql3);   
+
+
+$row = mysqli_fetch_array($query3);
+
+$valor=$row['valor'];
+
+$detalle=$row['descripcion'];
+
+$update="UPDATE `eventos` SET `tarifa` = '$tarifa', `valor`=$valor , `detalles`='$detalle' WHERE `eventos`.`id` = $id "; // realiza el update del valor y tarifa del evento
+
+mysqli_query($mysqli, $update);  
+
+
+echo "<br>";
+
+echo "  El dia $fecha ($dia) y hora $hora y vehiculo  $patente , categoria '$categoria' y portico '$portico'  ($detalle) tiene un valor de $ $valor que corresponde a la tarifa '$tarifa' ";
+
+
+
+
+?>
